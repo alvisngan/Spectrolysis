be1c85c325a17091ee11a8e1f167ae948f558c23
Alvis <alvischngan@gmail.com>
2024-11-12 23:13:40 -0500

    feat: added wasm support for file dialog

--- src/file_dialog.cpp
+++ src/file_dialog.cpp
@@ -7,8 +7,68 @@
 ///         I am planning to use SDL3, that would require
 ///         more code, hence a standalone file
 ///
+#ifdef __EMSCRIPTEN__ 
+#include <emscripten.h>
+#include <emscripten/html5.h>
+#else
 #include "tinyfiledialogs.h"
+#endif
 
+#ifdef __EMSCRIPTEN__ 
+// const char* fileDialogGetAudioPath(){ return "";}
+
+// Javascript, GROSS!!!!!!!
+
+
+EM_JS(void, openFileDialog, (char* buffer, int bufferSize), {
+    // JavaScript code:
+    Asyncify.handleSleep(function(wakeUp) {
+        var input = document.createElement('input');
+        input.type = 'file';
+
+        input.onchange = e => {
+            // Get only the first file
+            var file = e.target.files[0];
+
+            // No file selected
+            if (!file) {
+                buffer[0] = 0; // null-terminate buffer
+                wakeUp();
+                return;
+            }
+
+            var reader = new FileReader();
+            reader.onload = function(event) {
+                var data = new Uint8Array(event.target.result);
+                var filename = '/' + file.name;
+                FS.writeFile(filename, data);
+
+                // Includes the null terminator at the end
+                var lengthBytes = lengthBytesUTF8(filename) + 1;
+                if (lengthBytes > bufferSize) {
+                    console.error('Filename too long for buffer');
+                    buffer[0] = 0; // Indicate error
+                    wakeUp();
+                    return;
+                }
+
+                stringToUTF8(filename, buffer, bufferSize);
+                wakeUp();
+            };
+            reader.readAsArrayBuffer(file);
+        };
+        input.click();
+    });
+});
+
+static char filePathBuffer[1024]; // statically allocate memory
+const char* fileDialogGetAudioPath()
+{
+    openFileDialog(filePathBuffer,1024);
+    return filePathBuffer;
+}
+
+#else
 static constexpr int s_numFilterPatterns = 6;
 static const char* s_filterPatterns[s_numFilterPatterns] = {
     "*.wav", 
@@ -22,9 +82,10 @@ static const char* s_filterPatterns[s_numFilterPatterns] = {
 const char* fileDialogGetAudioPath()
 {
     return tinyfd_openFileDialog(nullptr,
-                                 nullptr,
-                                 s_numFilterPatterns,
-                                 s_filterPatterns,
-                                 nullptr,
-                                 0);
+                                nullptr,
+                                s_numFilterPatterns,
+                                s_filterPatterns,
+                                nullptr,
+                                0);
 }
+#endif
